eb08710ed0023e9b6e0673db780e8a11907b2eb2
Alcides Ticlla <bandycoot_17@hotmail.com>
2019-08-25 19:10:18 -0400

    adds Cell state test

--- test_Cell.py
+++ test_Cell.py
@@ -5,19 +5,30 @@ from cell import Cell
 class CellTest(unittest.TestCase):
 	
 	def test_cell_class(self):
-		cellTest = Cell(False)
+		cellTest = Cell(Cell.ALIVE)
 		self.assertIsInstance(cellTest, Cell, "Class Should be Cell")
 
-	def test_ALIVE_state(self):
+	def test_alive_state(self):
 		input_state = Cell.ALIVE
 		expected_state = 1
 		self.assertEqual(expected_state,input_state)
 
-	def test_DEATH_state (self):
+	def test_death_state (self):
 		input_state = Cell.DEATH
 		expected_state = 0
 		self.assertEqual(expected_state,input_state)		
 
+	def test_cell_alive(self):
+		cellTest = Cell(Cell.ALIVE)
+		input_state = cellTest.state
+		expected_state = Cell.ALIVE
+		self.assertEqual(input_state, expected_state)
+
+	def test_cell_death(self):
+		cellTest = Cell(Cell.DEATH)
+		input_state = cellTest.state
+		expected_state = Cell.DEATH
+		self.assertEqual(input_state, expected_state)
 
 
 

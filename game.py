b765fa9d145b6f30ddb23210444483f5551d0212
Alcides Ticlla <bandycoot_17@hotmail.com>
2019-08-26 15:15:32 -0400

    ädd get number of neighbours

--- game.py
+++ game.py
@@ -9,9 +9,6 @@ class Game:
 			for j in   range(len(board[0])):
 		 		self.board[i][j] =  Cell(board[i][j])
 		
-
-	
-
 	def __str__(self):
 		board_text = ""
 		for i in  range(len(self.board)):
@@ -22,9 +19,20 @@ class Game:
 
 		return board_text
 
-
 	def getCell(self,row, col):
 		return self.board[row][col]
+
+	def getNumberNeighbours(self, row, col):
+		right_neighbour = Cell.DEATH if (col + 1 >= len(self.board[0])) else self.board[row][col + 1].state
+		left_neighbour = Cell.DEATH if (col - 1 < 0) else self.board[row][col - 1].state
+		top_neighbour = Cell.DEATH if (row - 1 < 0) else self.board[row - 1][col].state
+		bottom_neighbour = Cell.DEATH if (row + 1 >= len(self.board)) else self.board[row + 1][col].state
+		top_corner_left_neighbour = Cell.DEATH if (row - 1 < 0 or col - 1 < 0) else self.board[row - 1][col - 1].state
+		top_corner_right_neighbour = Cell.DEATH if (row - 1 < 0 or col + 1 >= len(self.board[0])) else self.board[row - 1][col + 1].state
+		bottom_corner_left_neighbour = Cell.DEATH if (row + 1 >= len(self.board) or col - 1 < 0) else  self.board[row + 1][col - 1].state
+		bottom_corner_right_neighbour = Cell.DEATH if (row + 1 >= len(self.board) or col + 1 >= len(self.board[0])) else self.board[row + 1][col + 1].state
+
+		return right_neighbour + left_neighbour + top_neighbour + bottom_neighbour + top_corner_left_neighbour + top_corner_right_neighbour + bottom_corner_left_neighbour + bottom_corner_right_neighbour
 
 board = [
 		[Cell.ALIVE,Cell.DEATH,Cell.ALIVE],
--- test_game.py
+++ test_game.py
@@ -38,7 +38,76 @@ class GameTest(unittest.TestCase):
 		cell_location = game.getCell(0,0)
 		self.assertEqual(Cell.ALIVE,cell_location.state)		
 		cell_location = game.getCell(1,1)
-		self.assertEqual(Cell.DEATH,cell_location.state)		
+		self.assertEqual(Cell.DEATH,cell_location.state)	
+
+	def test_number_of_neighbours_happy_path(self):
+		initial_game_state = [
+		[Cell.ALIVE,Cell.ALIVE,Cell.ALIVE],
+		[Cell.ALIVE,Cell.ALIVE,Cell.ALIVE],
+		[Cell.ALIVE,Cell.ALIVE,Cell.ALIVE]
+		]
+		game = Game(initial_game_state)
+		neighbours = game.getNumberNeighbours(1,1)
+		expected_neighbours = 8
+		self.assertEqual(expected_neighbours,neighbours)		
+	
+	def test_number_of_neighbours_top_corner(self):
+		initial_game_state = [
+		[Cell.ALIVE,Cell.ALIVE,Cell.ALIVE],
+		[Cell.ALIVE,Cell.ALIVE,Cell.ALIVE],
+		[Cell.ALIVE,Cell.ALIVE,Cell.ALIVE]
+		]
+		game = Game(initial_game_state)
+		neighbours = game.getNumberNeighbours(0,0)
+		expected_neighbours = 3
+		self.assertEqual(expected_neighbours,neighbours)		
+
+		neighbours = game.getNumberNeighbours(0,2)
+		expected_neighbours = 3
+		self.assertEqual(expected_neighbours,neighbours)
+
+
+	def test_number_of_neighbours_bottom_corner(self):
+		initial_game_state = [
+		[Cell.ALIVE,Cell.ALIVE,Cell.ALIVE],
+		[Cell.ALIVE,Cell.ALIVE,Cell.ALIVE],
+		[Cell.ALIVE,Cell.ALIVE,Cell.ALIVE]
+		]
+		game = Game(initial_game_state)
+		neighbours = game.getNumberNeighbours(2,0)
+		expected_neighbours = 3
+		self.assertEqual(expected_neighbours,neighbours)		
+
+		neighbours = game.getNumberNeighbours(2,2)
+		expected_neighbours = 3
+		self.assertEqual(expected_neighbours,neighbours)
+
+	def test_number_of_neighbours_border(self):
+		initial_game_state = [
+		[Cell.ALIVE,Cell.ALIVE,Cell.ALIVE],
+		[Cell.ALIVE,Cell.ALIVE,Cell.ALIVE],
+		[Cell.ALIVE,Cell.ALIVE,Cell.ALIVE]
+		]
+		game = Game(initial_game_state)
+		neighbours = game.getNumberNeighbours(0,1)
+		expected_neighbours = 5
+		self.assertEqual(expected_neighbours,neighbours)		
+
+		neighbours = game.getNumberNeighbours(2,1)
+		expected_neighbours = 5
+		self.assertEqual(expected_neighbours,neighbours)
+
+		game = Game(initial_game_state)
+		neighbours = game.getNumberNeighbours(1,0)
+		expected_neighbours = 5
+		self.assertEqual(expected_neighbours,neighbours)		
+
+		neighbours = game.getNumberNeighbours(1,2)
+		expected_neighbours = 5
+		self.assertEqual(expected_neighbours,neighbours)
+
+
+
 
 
 if __name__ == '__main__':
